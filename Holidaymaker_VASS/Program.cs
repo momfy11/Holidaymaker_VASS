de1fc72823bc5fe23e15c855c8eae1b3a5fdfac7
Albin Martinsson <albinmartinsson@MacBook-Air-som-tillhor-Albin.local>
2024-11-29 13:00:04 +0100

    created main menu

--- Holidaymaker_VASS/Program.cs
+++ Holidaymaker_VASS/Program.cs
@@ -7,8 +7,11 @@ public class Program
         Database database = new Database();
         var createUser = new CreateUser(database.Connection());
        
-        int addressId = await createUser.AddAdress();
-        await createUser.AddUser(addressId);
+        //int addressId = await createUser.AddAdress();
+        //await createUser.AddUser(addressId);
+        
+        MainMenu mainmenu = new MainMenu();
+        MainMenu.Menu();
         
     }
     
