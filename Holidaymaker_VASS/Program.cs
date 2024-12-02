725097760a069232d329b9f7fc516abc71eb5abc
srecko radivojevic <Momfy86@gmail.com>
2024-12-02 14:57:57 +0100

    Added view_bookings
    Added the veiw bookings class and moved main menu options inside the
    loop so its shown after every return.

--- Holidaymaker_VASS/Program.cs
+++ Holidaymaker_VASS/Program.cs
@@ -4,14 +4,16 @@ public class Program
 {
     static async Task Main(string[] args)
     {
+        
+       
+        
         Database database = new Database();
+        var connection = database.Connection();
         var createUser = new CreateUser(database.Connection());
        
-        //int addressId = await createUser.AddAdress();
-        //await createUser.AddUser(addressId);
-        
+       
         MainMenu mainmenu = new MainMenu();
-        MainMenu.Menu();
+        await MainMenu.Menu(createUser, connection );
         
     }
     
