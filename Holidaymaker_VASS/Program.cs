7224c43ff160c5586d634c64d559aed45c2d810d
Sebbe <Sebbelarsson9601@gmail.com>
2024-12-03 15:03:12 +0100

    Added Sebbe stuff to MainMenu

--- Holidaymaker_VASS/Program.cs
+++ Holidaymaker_VASS/Program.cs
@@ -4,10 +4,6 @@ public class Program
 {
     static async Task Main(string[] args)
     {
-        Database database = new Database();
-        var createUser = new CreateUser(database.Connection());
-        var bookingMenu = new BookingMenu(database.Connection());
-        var searchBooking = new SearchBookingToEdit(database.Connection());
         
         // MainMenu mainmenu = new MainMenu();
         // mainmenu.Menu();
