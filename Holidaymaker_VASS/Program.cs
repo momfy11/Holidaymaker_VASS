0b27610ba27ae977e10ab81ed6f378a1ec0ea6a6
Sebbe <Sebbelarsson9601@gmail.com>
2024-12-03 09:14:19 +0100

    Main fix?

---
+++ Holidaymaker_VASS/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace app;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        Database database = new Database();
+        var createUser = new CreateUser(database.Connection());
+        //var bookingMenu = new BookingMenu(database.Connection());
+        
+        //await bookingMenu.BookingsMenu();
+
+        //int addressId = await createUser.AddAdress();
+        //await createUser.AddUser(addressId);
+        
+        //MainMenu mainmenu = new MainMenu();
+        //MainMenu.Menu();
+
+        // Accommodation accommodation = new(database.Connection());
+        // var accommodationMenu = new AccommodationMenu(accommodation);
+        // await accommodationMenu.Menu();
+        
+        //int addressId = await createUser.AddAdress();
+        //await createUser.AddUser(addressId);
+        //await createUser.AddGuest();
+    }
+    
+}
